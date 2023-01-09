49d4a5ab36ad93396d5b91dca490ef3d7e98a3d2
Bryan-ITSQMET <91750960+Bryan-ITSQMET@users.noreply.github.com>
2023-01-09 08:37:35 -0500

    crear los componentes del wheather con su respectiva funcionalidad y diseño

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
-  return d;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import WeatherApp from "./weather/weatherApp";
+const Main = () => {
+    return (
+        <main className="main">
+           <WeatherApp/>
+        </main>
+    );
+
+}
+export default Main;
